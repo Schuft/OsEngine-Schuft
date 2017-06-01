90201a553fc3313512b9a98607a6fc59249774da
Schuft <Schuft@bk.ru>
2017-06-01 23:53:00 +0300

    add

---
+++ project/OsEngine/OsData/OsDataUi.xaml.cs
@@ -0,0 +1,23 @@
+﻿/*
+ *Ваши права на использование кода регулируются данной лицензией http://o-s-a.net/doc/license_simple_engine.pdf
+*/
+
+using OsEngine.Market.Servers;
+
+namespace OsEngine.OsData
+{
+    /// <summary>
+    /// Логика взаимодействия для OsDataUi.xaml
+    /// </summary>
+    public partial class OsDataUi
+    {
+        public OsDataUi()
+        {
+            InitializeComponent();
+            ServerMaster.IsOsData = true;
+            new OsDataMaster(ChartHostPanel, HostLog, HostSource, HostSet, ComboBoxSecurity,ComboBoxTimeFrame,RectChart);
+            LabelOsa.Content = "V_" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        }
+
+    }
+}
